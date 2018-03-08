066cc4c92a74449add963c1808cba09a94182143
Cody Hennighan <cody.hennighan@gmail.com>
2018-03-08 23:54:27 +0800

    Rewrite sizzle, add scroll animation and improve buttons

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
 $(document).ready(function(){
-  $('.codeImg').attr("src","images/codeBlue.png"); //Default code button to blue
+  $('.heroButton').click(function(event){
+      $('html, body').animate({
+          scrollTop: $('.about').offset().top
+      }, 1000);
+      event.preventDefault();
+  });
 
-  $('.codeButton').click(function(){ //Code button pressed
-    $('.codeImg').attr("src","images/codeBlue.png");
-    $('.designImg').attr("src","images/design.png");
-    $('.pasImg').attr("src","images/passion.png");
-    $('.codeDets').show();
-    $('.designDets').hide();
-    $('.passionDets').hide();
+  $('#passion-button').click(function(event){
+      $('html, body').animate({
+          scrollTop: $('#work').offset().top
+      }, 1000);
+      event.preventDefault();
   });
-  $('.desButton').click(function(){ //Design button pressed
-    $('.codeImg').attr("src","images/code.png");
-    $('.designImg').attr("src","images/designBlue.png");
-    $('.pasImg').attr("src","images/passion.png");
-    $('.codeDets').hide();
-    $('.designDets').show();
-    $('.passionDets').hide();
-  });
-  $('.pasButton').click(function(){ //Passion button pressed
-    $('.codeImg').attr("src","images/code.png");
-    $('.designImg').attr("src","images/design.png");
-    $('.pasImg').attr("src","images/passionBlue.png");
-    $('.codeDets').hide();
-    $('.designDets').hide();
-    $('.passionDets').show();
+
+  $('#back-to-top-button').click(function(event){
+      $('html, body').animate({
+          scrollTop: $('#hero').offset().top
+      }, 500);
+      event.preventDefault();
   });
 });
